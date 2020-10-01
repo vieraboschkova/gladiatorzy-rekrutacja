e826f70f00b3b950262254e5b751039ab3653dcc
vieraboschkova <58532638+vieraboschkova@users.noreply.github.com>
2020-10-01 18:20:58 +0600

    init: add basic files

---
+++ arrayMethods.js
@@ -0,0 +1,23 @@
+// za pomocą metody .reduce wbudowanej w array 
+// odtwórz działanie innych metod:
+
+// - array.some
+// - array.every
+// - array.filter
+// - array.map
+
+function customArraySome () {
+
+}
+
+function customArrayEvery () {
+    
+}
+
+function customArrayFilter () {
+    
+}
+
+function customArrayMap () {
+    
+}
