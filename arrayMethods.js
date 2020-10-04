5eaf71229a744500595a7abc86c295da2fc1d629
vieraboschkova <58532638+vieraboschkova@users.noreply.github.com>
2020-10-04 21:08:05 +0600

    feat: add entries function logic

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,4 +1,3 @@
-const { duplicate, biggerThanThree, smallerOrEqualToThree, smallerThanTwenty } = require('./common');
 // Treść zadania #6 - metody wbudowane w Array
 // z uwagi na to, że JS jest jezykiem funkcyjnym warto opanować jego podstawowe metody
 
@@ -86,11 +85,37 @@ function everyFn(array, callback) {
 
 /* **** SOME **** */
 
-function someFn(array, callback){}
+function someFn(array, callback){
+    let hasSome = false;
+    if (!Array.isArray(array)) {
+        throw new TypeError('not a valid array');
+    } else {
+        for (let i = 0; i < array.length; i += 1) {
+            console.log('iterating: ' + i)
+            if (!(i in array)) {
+                console.log('was empty');
+            } else if(callback(array[i])) {
+                hasSome = true;
+                console.log(hasSome + 'was here')
+                i = array.length - 1;
+            }
+        }
+    }
+    console.log(hasSome)
+    return hasSome;
+}
 
 /* **** ENTRIES **** */
 
-function entriesFn(array){}
+function entriesFn(array){
+    let newArray = new Array(array.length);
+    const keys = [...array.keys()];
+    const values = [...array.values()];
+    for (i in keys) {
+        newArray[i] = [keys[i], values[i]];
+    }
+    return newArray;
+}
 
 
 
@@ -98,17 +123,26 @@ function entriesFn(array){}
 /* ***** SIMPLE TESTS ***** */
 
 const testedValue = 
-    // mapFn(common.numbers, duplicate);
-    // mapFn(common.mixed, duplicate);
-    // mapFn(common.emptyArray, duplicate);
+    // mapFn(common.numbers, common.duplicate);
+    // mapFn(common.mixed, common.duplicate);
+    // mapFn(common.emptyArray, common.duplicate);
     // mapFn(1)
-    // filterFn(common.numbers, biggerThanThree)
-    // filterFn(common.emptyArray, biggerThanThree)
+    // filterFn(common.numbers, common.biggerThanThree)
+    // filterFn(common.emptyArray, common.biggerThanThree)
     // filterFn(common.mixed, ((i) => i === false))
-    // filterFn(common.mixed, smallerOrEqualToThree)
-    // everyFn(common.numbers, smallerOrEqualToThree)
-    everyFn(common.numbers, smallerThanTwenty)
-    // everyFn(common.numbers, biggerThanThree)
+    // filterFn(common.mixed,  common.smallerOrEqualToThree)
+    // everyFn(common.numbers,  common.smallerOrEqualToThree)
+    // everyFn(common.numbers, common.smallerThanTwenty)
+    // everyFn(common.numbers, common.biggerThanThree)
+    // someFn(common.numbers, common.smallerThanTwenty)
+    // someFn(common.numbers, common.biggerThanTwenty)
+    // someFn(common.emptyArray, common.biggerThanTwenty)
+    // someFn(common.mixed,  common.smallerOrEqualToThree)
+    // someFn(common.mixed, common.biggerThanTwenty)
+    // entriesFn(common.numbers)
+    // entriesFn(common.emptyArray)
+    // entriesFn(common.mixed)
+    entriesFn(common.falsies)
 
 const desiredValue = 
     // common.numbers.map(duplicate);
@@ -117,10 +151,30 @@ const desiredValue =
     // common.numbers.filter(biggerThanThree)
     // common.emptyArray.filter(biggerThanThree)
     // common.mixed.filter(((i) => i === false))
-    // common.mixed.filter(smallerOrEqualToThree)
-    // common.numbers.every(smallerOrEqualToThree)
-    common.numbers.every(smallerThanTwenty)
+    // common.mixed.filter( common.smallerOrEqualToThree)
+    // common.numbers.every( common.smallerOrEqualToThree)
+    // common.numbers.every(smallerThanTwenty)
     // common.numbers.every(biggerThanThree)
+    // common.numbers.some(smallerThanTwenty)
+    // common.numbers.some(biggerThanTwenty)
+    // common.emptyArray.some(biggerThanTwenty)
+    // common.mixed.some( common.smallerOrEqualToThree)
+    // common.mixed.some(common.biggerThanTwenty)
+    // common.numbers.entries()
+    // common.emptyArray.entries()
+    // common.mixed.entries()
+    common.falsies.entries()
 
-console.log('TESTED: ' + testedValue);
-console.log('SHOULD BE: ' + desiredValue);
+
+// console.log('TESTED: ' + testedValue);
+// console.log('SHOULD BE: ' + desiredValue);
+// console.log('TESTED: ' + typeof testedValue);
+// console.log('SHOULD BE: ' + typeof desiredValue);
+
+for (let e of testedValue) {
+    console.log('TESTED: ' +  e);
+}
+
+for (let e of desiredValue) {
+    console.log('SHOULD BE: ' +  e);
+}
