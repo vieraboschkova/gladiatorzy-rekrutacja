e9f6cffad337008b06bb416493cf5627ea9b6083
vieraboschkova <58532638+vieraboschkova@users.noreply.github.com>
2020-10-06 23:33:20 +0600

    fix: change the way to check the accumulator

--- masterReduce.js
+++ masterReduce.js
@@ -23,20 +23,16 @@ function customArraySome (arr, callback) {
     } else {
         const newArray = common.clone(arr);                         // clone array without reference
         console.log(newArray)    
-        return newArray.reduce((accumulator, currentValue, index, newArray) => {
+        return newArray.reduce((accumulator, currentValue, index, newArray) => { // start reduce function
             console.log(currentValue + ":" + index)
-            if (callback(accumulator, index, newArray)) {
-                console.log(accumulator + " :accumulator was true at index: " + index)
-                newArray.length = 0;
+            if (callback(currentValue, index, newArray) || callback(accumulator, index, newArray)) { 
+                                                                    // if accumulator or current value is true
+                console.log(accumulator + ' :acc: cur: ' + currentValue + ":at:" + index)
+                newArray.length = index;                            // shorten itirated array to stop reduce
                 console.log(newArray)
-                return true;
-            } else if (callback(currentValue, index, newArray)) {
-                console.log(currentValue + ":" + index)
-                newArray.length = index - 1;
-                console.log(newArray)
-                return true;
+                return true;                                        // some returns true
             }
-            return false;
+            return false;                                           // if no true values found, returns false
         });
     }
 }
@@ -45,32 +41,28 @@ function customArraySome (arr, callback) {
 /* implement custom array.every */
 
 function customArrayEvery (arr, callback) {
-    if (!Array.isArray(arr)) {                            // check if array is actually an array
+    if (!Array.isArray(arr)) {                                      // check if array is actually an array
         throw new TypeError('not a valid array');
-    } else if (typeof callback !== 'function') {            // check if function is type function
+    } else if (typeof callback !== 'function') {                    // check if function is type function
         throw new TypeError(callback + ' is not a function');
-    } else if (arr.length === 0) {                        // check if called on an empty array
-        return true;                                        // return true
+    } else if (arr.length === 0) {                                  // check if called on an empty array
+        return true;                                                // return true
     } else {
         // const newArray = [...arr];
-        const newArray = common.clone(arr);
+        const newArray = common.clone(arr);                         // clone array without reference
         console.log(newArray)
-        return newArray.reduce((accumulator, currentValue, index, newArray) => {
+        return newArray.reduce((accumulator, currentValue, index, newArray) => { // start reduce function
             console.log(currentValue + ":" + index)
-            if (!callback(accumulator, index, newArray)) {
-                console.log('acculmulator is false' + accumulator + ":" + (index -1))
-                newArray.length = 0;
-                console.log(newArray)
-                return false;
-            } else if (index in newArray) {
-                if (!callback(currentValue, index, newArray)) {
+            if (index in newArray) {
+                if ((!callback(currentValue, index, newArray)) || (!callback(accumulator, index, newArray))) { 
+                                                                    // if accumulator or current value is false
                     console.log(currentValue + " : was false at index: " + index)
-                    newArray.length = index - 1;
+                    newArray.length = index;                        // shorten itirated array to stop reduce
                     console.log(newArray)
-                    return false;
+                    return false;                                   // every returns false
                 }
             }
-            return true;
+            return true;                                            // if no false values, every returns true
         });
     }
 }
@@ -111,14 +103,14 @@ let testedValue =
     // customArrayEvery(common.alphanumeric, common.smallerThanTwenty)
     // customArrayEvery(common.numbers, common.smallerThanTwenty)
     // customArrayEvery(common.numbers, common.biggerThanThree)
-    // customArrayEvery(common.numbers, common.smallerOrEqualToThree)
+    customArrayEvery(common.numbers, common.smallerOrEqualToThree)
     // customArrayEvery(common.emptyArray, common.biggerThanThree)
     
     // customArraySome(common.numbers, common.smallerOrEqualToThree)
     // customArraySome(common.numbers, common.biggerThanTwenty)
     // customArraySome(common.alphanumeric, common.biggerThanTwenty)
-    customArraySome(common.alphanumeric, common.smallerOrEqualToThree)
-
+    // customArraySome(common.alphanumeric, common.smallerOrEqualToThree)
+    // customArraySome([0,1,2,3,4,5,6], common.biggerThanThree)
     // common.numbers.filterUsingReduce(common.biggerThanThree)
     // common.numbers.someUsingReduce(common.biggerThanThree)
     // common.numbers.someUsingReduce(common.biggerThanTwenty)
@@ -132,11 +124,12 @@ let desiredValue =
     // common.numbers.some(common.smallerOrEqualToThree)
     // common.numbers.some(common.biggerThanTwenty)
     // common.alphanumeric.some(common.biggerThanTwenty)
-    common.alphanumeric.some(common.smallerOrEqualToThree)
+    // common.alphanumeric.some(common.smallerOrEqualToThree)
+
     // common.alphanumeric.every(common.smallerThanTwenty)
     // common.numbers.every(common.smallerThanTwenty)
     // common.numbers.every(common.biggerThanThree)
-    // common.numbers.every(common.smallerOrEqualToThree)
+    common.numbers.every(common.smallerOrEqualToThree)
     // common.emptyArray.every(common.biggerThanThree)
     
 
